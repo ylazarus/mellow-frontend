01ef8a778e5b2c71b3eb603a6a961a815392b623
Zviki_Zaks <zvikizax@gmail.com>
2022-03-23 19:42:32 +0200

    save group

--- src/components/board-group.vue
+++ src/components/board-group.vue
@@ -1,9 +1,16 @@
 <template>
   <section class="group-container">
     <div contenteditable="true" @blur="saveTitle">{{ group.title }}</div>
-    <task-preview v-for="task in group.tasks" :key="task.id" :task="task" :groupId="group.id" />
-    <button class="btn" v-if="!isAdding" @click="openAddTask">Add a card</button>
-    <div  v-else class="add-task-container">
+    <task-preview
+      v-for="task in group.tasks"
+      :key="task.id"
+      :task="task"
+      :groupId="group.id"
+    />
+    <button class="btn" v-if="!isAdding" @click="openAddTask">
+      Add a card
+    </button>
+    <div v-else class="add-task-container">
       <input
         @blur="saveIfTxt"
         type="textarea"
@@ -11,9 +18,8 @@
         placeholder="Enter a title for this card..."
       />
       <div class="add-task-buttons-container flex">
-
-      <button class="btn" @click="addTask">Save</button>
-      <button class="btn" @click="clearForm">X</button>
+        <button class="btn" @click="addTask">Save</button>
+        <button class="btn" @click="clearForm">X</button>
       </div>
     </div>
   </section>
@@ -31,43 +37,46 @@ export default {
   data() {
     return {
       isAdding: false,
-      newTaskTitle: '',
+      newTaskTitle: "",
     };
   },
   methods: {
-      saveIfTxt(){
-          if (this.newTaskTitle) this.addTask()
-          else this.clearForm()
-      },
-      clearForm(){
-          this.isAdding = false
-          this.newTaskTitle = ''
-      },
-      openAddTask(){
-          this.isAdding = true
-        //   console.log(this.$refs);
-        //   this.$refs.input.focus()
-      },
+    saveIfTxt() {
+      if (this.newTaskTitle) this.addTask();
+      else this.clearForm();
+    },
+    clearForm() {
+      this.isAdding = false;
+      this.newTaskTitle = "";
+    },
+    openAddTask() {
+      this.isAdding = true;
+      //   console.log(this.$refs);
+      //   this.$refs.input.focus()
+    },
     addTask() {
-        console.log('adding task');
+      console.log("adding task");
       if (!this.newTaskTitle) {
         this.isAdding = false;
         return;
       } else {
+        const task = {};
+        task.title = this.newTaskTitle;
         this.$emit("saveGroup", {
           groupId: this.group.id,
           type: "addTask",
-          task: this.newTaskTitle,
+          newValue: task,
         });
-        this.clearForm()
+        this.clearForm();
       }
     },
     saveTitle(ev) {
       const newTitle = ev.currentTarget.textContent;
+      console.log(newTitle);
       this.$emit("saveGroup", {
         groupId: this.group.id,
         type: "saveGroupTitle",
-        newTitle,
+        newValue: newTitle,
       });
     },
   },
