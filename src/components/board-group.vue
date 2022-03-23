5e0f32ecef3f1c668d2567ae7ecf573fc9affec6
Yoni Lazarus <ylazarus@gmail.com>
2022-03-23 12:49:37 +0200

    added scss files

--- src/components/board-group.vue
+++ src/components/board-group.vue
@@ -1,7 +1,7 @@
 <template>
 <section class="flex">
 
-  <p>Groups!!!</p>
+  <div contenteditable="true" @blur="saveTitle">Groups!!!</div>
   <task-preview v-for="task in group.tasks" :key="task.id" :task="task" />
 </section>
 </template>
@@ -14,6 +14,13 @@ export default {
     },
     components:{
         taskPreview
+    },
+    methods:{
+        saveTitle(ev){
+            // console.log(ev);
+            console.log(ev.currentTarget.textContent);
+            //emit group id type:saveGroupTitle value
+        }
     }
 
 }
