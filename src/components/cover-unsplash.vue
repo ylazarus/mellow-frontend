0351860f16d2bfa79076a2b0b1150b6b2d5cd3b7
Yoni Lazarus <ylazarus@gmail.com>
2022-03-27 17:02:07 +0300

    start of unsplash

---
+++ src/components/cover-unsplash.vue
@@ -0,0 +1,26 @@
+<template>
+  
+</template>
+
+<script>
+import { getCurrentInstance, onMounted } from 'vue'
+export default {
+  name: 'cover-unsplash',
+ 
+  setup(props, context) {
+    const instance = getCurrentInstance()
+    const globalProperties = instance.appContext.config.globalProperties
+ 
+    onMounted(async () => {
+      var image = await globalProperties.$unsplash.getPhoto('l3N9Q27zULw')
+      var urls = image.urls || []
+      var links = image.links || []
+      var user = image.user || {}
+    })
+  }
+}
+</script>
+
+<style>
+
+</style>
