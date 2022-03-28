f37e08b5ada75e926c281c8702434eee2fbbee79
Yoni Lazarus <ylazarus@gmail.com>
2022-03-29 00:05:58 +0300

    functional unsplash

--- src/services/socket.service.js
+++ src/services/socket.service.js
@@ -6,7 +6,7 @@ export const SOCKET_EVENT_REVIEW_ADDED = 'review-added';
 export const SOCKET_EVENT_REVIEW_ABOUT_YOU = 'review-about-you';
 
 
-const baseUrl = (process.env.NODE_ENV === 'production')? '' : '//localhost:3030'
+const baseUrl = (process.env.NODE_ENV === 'production')? '' : '//localhost:3000'
 export const socketService = createSocketService()
 // export const socketService = createDummySocketService()
 
