c63de228784b2f11331698f520fca66c17573a14
Almogbb <almogbb1990@gmail.com>
2022-03-30 21:15:06 +0300

    start of home page

--- src/views/home-page.vue
+++ src/views/home-page.vue
@@ -2,7 +2,12 @@
   <section class="home-page-container">
     <h1>Mello</h1>
     <router-link to="/board">To WorkSpace</router-link>
-    <img />
+    <!-- <img src="src/assets/svgs/groups.svg" /> -->
+    <!-- <h2>Mello</h2> -->
+    <h3 class="hero-title">let us do the management for you</h3>
+    <div class="hero-container">
+      <img class="hero-img" src="src/assets/imgs/hero-mello.jpg" />
+    </div>
   </section>
 </template>
 
