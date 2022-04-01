93661076962b2593afa8a96ce1e6afc709dddb44
Almogbb <almogbb1990@gmail.com>
2022-04-01 17:25:00 +0300

    fix fix fix

--- src/components/user-avatar.vue
+++ src/components/user-avatar.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="user-avatar">
-    <span class="user-avatar-guest flex" v-if="user.username === 'Guest'"
-      >}</span
-    >
+    <span class="user-avatar-guest flex" v-if="user.username === 'Guest'"></span>
     <img class="user-avatar-img" v-else-if="user.imgUrl" :src="imgAvatar" />
     <span class="user-avatar-name flex" v-else>{{ nameAvatar }}</span>
   </div>
