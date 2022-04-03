ace0e2f28422c4b71c8bcd8a47c06ddc53c2a108
Yoni Lazarus <ylazarus@gmail.com>
2022-04-03 09:36:17 +0300

    style improvements

--- src/views/work-space.vue
+++ src/views/work-space.vue
@@ -1,18 +1,17 @@
 <template>
-  <main class="main-layout">
+  <main class="workspace-page-container">
     <!-- <div class="side-bar">
       <div>
         <h3>boards</h3>
         <h3>members</h3>
       </div>
     </div>-->
-    <section class="all-boards-container">
-      <!-- <h1>Work Space</h1> -->
+      <section class="all-boards-container">
+        <!-- <h1>Work Space</h1> -->
 
-      <section>
-        <h1>Favorite boards</h1>
-        <ul class="favorite-boards flex">
-          <div class="boards-container flex">
+        <section>
+          <h1>Favorite boards</h1>
+          <ul class="favorite-boards flex">
             <li
               v-for="board in favoriteBoards"
               :key="board._id"
@@ -22,46 +21,48 @@
             >
               <h2 class="favorite-board-title">{{ board.title }}</h2>
             </li>
+          </ul>
+        </section>
+
+        <h3 class="all-boards-title">All boards</h3>
+        <article class="saved-boards">
+          <div
+            v-for="board in boards"
+            :key="board._id"
+            @click="toBoardDetails(board._id)"
+            class="board-preview img-pos style-font"
+            :style="{ backgroundImage: `url(${board.style?.bgImg || ''})` }"
+          >
+            <h2 class="title">{{ board.title }}</h2>
+
+            <a class="star-container" @click.stop="toggleFavorite(board._id)">
+              <img
+                class="ws-full-star"
+                v-if="board.isFavorite"
+                src="../assets/icons/full-star.png"
+              />
+              <img
+                class="ws-empty-star"
+                v-else
+                src="../assets/icons/empty-star.png"
+              />
+              <!-- <img src="../assets/icons/empty-star.png" /> -->
+              <!-- <img :src="changeImgUrl" /> -->
+            </a>
+            <!-- <button class="remove-board-button" @click.stop="removeBoard(board._id)">remove</button> -->
           </div>
-        </ul>
+          <button
+            @click="isCreateBoard = true"
+            class="board-preview justify-center add-card"
+          >
+            Create new board
+          </button>
+          <create-board
+            v-if="isCreateBoard"
+            @closeCmp="isCreateBoard = false"
+          />
+        </article>
       </section>
-
-      <h3>All boards</h3>
-      <article class="saved-boards">
-        <div
-          v-for="board in boards"
-          :key="board._id"
-          @click="toBoardDetails(board._id)"
-          class="board-preview img-pos style-font"
-          :style="{ backgroundImage: `url(${board.style?.bgImg || ''})` }"
-        >
-          <h2 class="title">{{ board.title }}</h2>
-
-          <a class="star-container" @click.stop="toggleFavorite(board._id)">
-            <img
-              class="ws-full-star"
-              v-if="board.isFavorite"
-              src="../assets/icons/full-star.png"
-            />
-            <img
-              class="ws-empty-star"
-              v-else
-              src="../assets/icons/empty-star.png"
-            />
-            <!-- <img src="../assets/icons/empty-star.png" /> -->
-            <!-- <img :src="changeImgUrl" /> -->
-          </a>
-          <!-- <button class="remove-board-button" @click.stop="removeBoard(board._id)">remove</button> -->
-        </div>
-        <button
-          @click="isCreateBoard = true"
-          class="board-preview justify-center add-card"
-        >
-          Create new board
-        </button>
-        <create-board v-if="isCreateBoard" @closeCmp="isCreateBoard = false" />
-      </article>
-    </section>
   </main>
 </template>
 
