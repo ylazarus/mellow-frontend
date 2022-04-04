2219acc43da3e6d1623eda6d0b8e870f001918c6
Yoni Lazarus <ylazarus@gmail.com>
2022-04-04 21:30:27 +0300

    added touch events library, not yet in use

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from './store'
 import { focusDirective } from './directives'
 import VCalendar from 'v-calendar';
 import Unsplash from 'vue-unsplash'
+import Vue3TouchEvents from "vue3-touch-events";
+
 
 import "./styles/scss/main.scss"
 
@@ -14,6 +16,7 @@ import ElementPlus from 'element-plus'
 const app = createApp(App)
 
 app.directive('focus', focusDirective)
+app.use(Vue3TouchEvents);
 app.use(VCalendar, {})
 app.use(Unsplash, { accessKey: 'Y2X6Y_wdMpqvaYX_4jgO-dOBqVAsQMQpihsIFNOAX5E', secret: "W5FeRrlFbgAz3i3d54dHnDm9okVhUeUIXZ6TyvZC_H4" })
 app.use(ElementPlus)
